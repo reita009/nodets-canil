19440938d134a647756639445ab7d2890946fc89
Julio Santana <julio.python777@gmail.com>
2023-03-29 09:13:02 -0300

    criando model

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,17 +1,43 @@
 import { Request, Response } from "express";
 
+import { createMenuObject } from "../helpers/createMenuObject";
+
 export const home = (req: Request, res: Response) => {
-  res.send("ola sou a Home!");
+  res.render("pages/page", {
+    menu: createMenuObject("all"),
+    banner: {
+      title: "Todos os animais",
+      background: "allanimals.jpg",
+    },
+  });
 };
 
 export const dogs = (req: Request, res: Response) => {
-  res.send("ola sou a dogs!");
+  res.render("pages/page", {
+    menu: createMenuObject("dog"),
+    banner: {
+      title: "cachorros",
+      background: "banner_dog.jpg",
+    },
+  });
 };
 
 export const cats = (req: Request, res: Response) => {
-  res.send("ola sou cats!");
+  res.render("pages/page", {
+    menu: createMenuObject("cat"),
+    banner: {
+      title: "Gatos",
+      background: "banner_cat.jpg",
+    },
+  });
 };
 
 export const fishes = (req: Request, res: Response) => {
-  res.send("ola sou a fishes!");
+  res.render("pages/page", {
+    menu: createMenuObject("fish"),
+    banner: {
+      title: "Peixes",
+      background: "banner_fish.jpg",
+    },
+  });
 };
