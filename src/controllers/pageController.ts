e54d08aeff5e94cf917e1ca293fb9e8b452c2ff2
Julio Santana <julio.python777@gmail.com>
2023-03-29 09:59:09 -0300

    final project 1.0

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,43 +1,52 @@
 import { Request, Response } from "express";
 
+import { pet } from "../models/pet";
 import { createMenuObject } from "../helpers/createMenuObject";
 
 export const home = (req: Request, res: Response) => {
+  let list = pet.getAll();
   res.render("pages/page", {
     menu: createMenuObject("all"),
     banner: {
       title: "Todos os animais",
       background: "allanimals.jpg",
     },
+    list,
   });
 };
 
 export const dogs = (req: Request, res: Response) => {
+  let list = pet.getFromType("dog");
   res.render("pages/page", {
     menu: createMenuObject("dog"),
     banner: {
       title: "cachorros",
       background: "banner_dog.jpg",
     },
+    list,
   });
 };
 
 export const cats = (req: Request, res: Response) => {
+  let list = pet.getFromType("cat");
   res.render("pages/page", {
     menu: createMenuObject("cat"),
     banner: {
       title: "Gatos",
       background: "banner_cat.jpg",
     },
+    list,
   });
 };
 
 export const fishes = (req: Request, res: Response) => {
+  let list = pet.getFromType("fish");
   res.render("pages/page", {
     menu: createMenuObject("fish"),
     banner: {
       title: "Peixes",
       background: "banner_fish.jpg",
     },
+    list,
   });
 };
--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,3 +1,21 @@
 import { Request, Response } from "express";
 
-export const search = (req: Request, res: Response) => {};
+import { pet } from "../models/pet";
+import { createMenuObject } from "../helpers/createMenuObject";
+
+export const search = (req: Request, res: Response) => {
+  let query: string = req.query.q as string;
+
+  if (!query) {
+    res.redirect("/");
+    return;
+  }
+
+  let list = pet.gerFromName(query);
+
+  res.render("pages/page", {
+    menu: createMenuObject(""),
+    list,
+    query,
+  });
+};
